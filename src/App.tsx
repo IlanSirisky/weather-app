a0363af528c39db8a2ab32309252abdbe066fd23
IlanSirisky <ilans@moveohls.com>
2024-05-09 15:31:28 +0300

    dependencies and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header>
+        <h1>Weather App</h1>
+      </header>
+      <h2>Location: Tel Aviv</h2>
+      <p>Temperature: 28°C</p>
+      <p>SearchBar</p>
+      <button>Search</button>
+      <ul>
+        <li>History item 1</li>
+        <li>History item 2</li>
+        <li>History item 3</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
