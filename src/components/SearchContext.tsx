ec3474d2a57809645ba6f4d335d5dd97e205d6cf
IlanSirisky <ilans@moveohls.com>
2024-05-12 17:15:54 +0300

    Implemented Search History component
    Added Search History component, Search Item component.
    Updated Search Context to use local storage for search history.
    Added button to clear search history.

--- src/components/SearchContext.tsx
+++ src/components/SearchContext.tsx
@@ -1,9 +1,10 @@
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, useEffect, ReactNode } from "react";
 
 interface SearchContextType {
   searchTerm: string;
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
   searchHistory: string[];
+  setSearchHistory: React.Dispatch<React.SetStateAction<string[]>>;
   addToSearchHistory: (term: string) => void;
 }
 
@@ -11,6 +12,7 @@ export const SearchContext = createContext<SearchContextType>({
   searchTerm: "",
   setSearchTerm: () => {},
   searchHistory: [],
+  setSearchHistory: () => {},
   addToSearchHistory: () => {},
 });
 
@@ -18,7 +20,21 @@ export const SearchProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+  const [searchHistory, setSearchHistory] = useState<string[]>(() => {
+    const storedSearchHistory = localStorage.getItem("searchHistory");
+    return storedSearchHistory ? JSON.parse(storedSearchHistory) : [];
+  });
+
+  useEffect(() => {
+    const storedSearchHistory = localStorage.getItem("searchHistory");
+    if (storedSearchHistory) {
+      setSearchHistory(JSON.parse(storedSearchHistory));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  }, [searchHistory]);
 
   const addToSearchHistory = (term: string) => {
     setSearchHistory((prevHistory) => [term, ...prevHistory]);
@@ -26,7 +42,13 @@ export const SearchProvider: React.FC<{ children: ReactNode }> = ({
 
   return (
     <SearchContext.Provider
-      value={{ searchTerm, setSearchTerm, searchHistory, addToSearchHistory }}>
+      value={{
+        searchTerm,
+        setSearchTerm,
+        searchHistory,
+        setSearchHistory,
+        addToSearchHistory,
+      }}>
       {children}
     </SearchContext.Provider>
   );
