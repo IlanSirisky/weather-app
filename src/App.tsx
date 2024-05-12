10dc6aa1b6ce8ad38aca508d7ac3ca1f99985433
IlanSirisky <ilans@moveohls.com>
2024-05-12 13:38:12 +0300

    Implemented SearchBar component
    Implemented SearchBar component, exported fetchWeather function and added reactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Weather } from "./types/weather";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Button from "./components/Button/Button";
-
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY || "";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  const [weather, setWeather] = useState<Weather | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://api.weatherapi.com/v1/current.json",
-          {
-            params: {
-              key: apiKey,
-              q: "tel aviv",
-            },
-          }
-        );
-        setWeather(response.data);
-      } catch (error) {
-        console.error("Error fetching weather data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div>
+    <>
       <Header />
-      {weather ? (
-        <>
-          <h2>Location: {weather.location.name}</h2>
-          <p>Temperature: {weather.current.temp_c}°C</p>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
-      <p>SearchBar</p>
-      <Button text="Search" onClick={() => console.log("clicked")} />
+      <SearchBar />
       <ul>
         <li>History item 1</li>
         <li>History item 2</li>
         <li>History item 3</li>
       </ul>
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/utils/http.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+import { WeatherData } from "../types/weather";
+
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY || "";
+
+export const fetchWeather = async ({ signal, searchTerm }: { signal: AbortSignal, searchTerm: string }) => {
+  try {
+    const params: AxiosRequestConfig = {
+      params: {
+        key: apiKey,
+        q: searchTerm,
+      },
+      signal,
+    };
+
+    const response = await axios.get(
+      "http://api.weatherapi.com/v1/current.json",
+      params
+    );
+    const weather: WeatherData = response.data;
+    return weather;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+};
